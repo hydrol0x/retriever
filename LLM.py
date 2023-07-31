ae56051c8462311d3793a21dda8ddc984b8f9da1
Jacob.Ryabinky@mpfi.org <mrjacobry@gmail.com>
2023-07-30 22:58:07 -0400

    run loop

--- LLM.py
+++ LLM.py
@@ -6,6 +6,21 @@ from palm import handle_chat
 
 # Give PaLM the users command -> PaLM decides on a list of actions to run 
 
+# ! this might just be too hard for LLM!
+async def get_program_name(name: str, program_list: list) -> str:
+    prompt=f"""
+Here are a list of programs on the user's computer. 
+{program_list}
+
+
+Return the program name from the list above that most closely matches {name}
+"""
+
+    print(prompt)
+    resp = await handle_chat(prompt)
+    resp = resp["response"]
+    return resp
+
 from prompts import user_command_intent_prompt
 async def get_user_actions(user_command:str) -> list:
     prompt=f"""
